0cc13014d7481efadb9d0bcb539fa6673afe7609
Kuzemko Alexsandr <1382812+Kuzma30@users.noreply.github.com>
2022-04-29 11:31:51 +0300

    New translations Web.ts (Russian)

--- src/Mod/Web/Gui/Resources/translations/Web_ru.ts
+++ src/Mod/Web/Gui/Resources/translations/Web_ru.ts
@@ -5,152 +5,152 @@
     <name>CmdWebBrowserBack</name>
     <message>
       <source>Web</source>
-      <translation>Интернет</translation>
+      <translation type="unfinished">Web</translation>
     </message>
     <message>
       <source>Previous page</source>
-      <translation>Предыдущая страница</translation>
+      <translation type="unfinished">Previous page</translation>
     </message>
     <message>
       <source>Go back to the previous page</source>
-      <translation>Вернуться на предыдущую страницу</translation>
+      <translation type="unfinished">Go back to the previous page</translation>
     </message>
   </context>
   <context>
     <name>CmdWebBrowserNext</name>
     <message>
       <source>Web</source>
-      <translation>Интернет</translation>
+      <translation type="unfinished">Web</translation>
     </message>
     <message>
       <source>Next page</source>
-      <translation>Следующая страница</translation>
+      <translation type="unfinished">Next page</translation>
     </message>
     <message>
       <source>Go to the next page</source>
-      <translation>Перейти на следующую страницу</translation>
+      <translation type="unfinished">Go to the next page</translation>
     </message>
   </context>
   <context>
     <name>CmdWebBrowserRefresh</name>
     <message>
       <source>Web</source>
-      <translation>Интернет</translation>
+      <translation type="unfinished">Web</translation>
     </message>
     <message>
       <source>Refresh web page</source>
-      <translation>Обновить страницу</translation>
+      <translation type="unfinished">Refresh web page</translation>
     </message>
   </context>
   <context>
     <name>CmdWebBrowserSetURL</name>
     <message>
       <source>Web</source>
-      <translation>Интернет</translation>
+      <translation type="unfinished">Web</translation>
     </message>
     <message>
       <source>Set URL</source>
-      <translation>Установить URL</translation>
+      <translation type="unfinished">Set URL</translation>
     </message>
   </context>
   <context>
     <name>CmdWebBrowserStop</name>
     <message>
       <source>Web</source>
-      <translation>Интернет</translation>
+      <translation type="unfinished">Web</translation>
     </message>
     <message>
       <source>Stop loading</source>
-      <translation>Остановить загрузку</translation>
+      <translation type="unfinished">Stop loading</translation>
     </message>
   </context>
   <context>
     <name>CmdWebBrowserZoomIn</name>
     <message>
       <source>Web</source>
-      <translation>Интернет</translation>
+      <translation type="unfinished">Web</translation>
     </message>
     <message>
       <source>Zoom in</source>
-      <translation>Увеличить</translation>
+      <translation type="unfinished">Zoom in</translation>
     </message>
   </context>
   <context>
     <name>CmdWebBrowserZoomOut</name>
     <message>
       <source>Web</source>
-      <translation>Интернет</translation>
+      <translation type="unfinished">Web</translation>
     </message>
     <message>
       <source>Zoom out</source>
-      <translation>Уменьшить</translation>
+      <translation type="unfinished">Zoom out</translation>
     </message>
   </context>
   <context>
     <name>CmdWebOpenWebsite</name>
     <message>
       <source>Web</source>
-      <translation>Интернет</translation>
+      <translation type="unfinished">Web</translation>
     </message>
     <message>
       <source>Open website...</source>
-      <translation>Открыть сайт...</translation>
+      <translation type="unfinished">Open website...</translation>
     </message>
     <message>
       <source>Opens a website in FreeCAD</source>
-      <translation>Открывает сайт в FreeCAD</translation>
+      <translation type="unfinished">Opens a website in FreeCAD</translation>
     </message>
   </context>
   <context>
     <name>QObject</name>
     <message>
       <source>Browser</source>
-      <translation>Браузер</translation>
+      <translation type="unfinished">Browser</translation>
     </message>
     <message>
       <source>File does not exist!</source>
-      <translation>Файл не существует!</translation>
+      <translation type="unfinished">File does not exist!</translation>
     </message>
   </context>
   <context>
     <name>WebGui::BrowserView</name>
     <message>
       <source>Error</source>
-      <translation>Ошибки</translation>
+      <translation type="unfinished">Error</translation>
     </message>
     <message>
       <source>There were errors while loading the file. Some data might have been modified or not recovered at all. Look in the report view for more specific information about the objects involved.</source>
-      <translation>Произошли ошибки при загрузке файла. Возможно, некоторые данные были изменены или не восстановлены. Посмотрите в окно отчета для более конкретной информации о затронутых объектах.</translation>
+      <translation type="unfinished">There were errors while loading the file. Some data might have been modified or not recovered at all. Look in the report view for more specific information about the objects involved.</translation>
     </message>
     <message>
       <source>There were serious errors while loading the file. Some data might have been modified or not recovered at all. Saving the project will most likely result in loss of data.</source>
-      <translation>При загрузке файла возникли серьезные ошибки. Некоторые данные могли быть изменены или не восстановлены вообще. Сохранение проекта скорее всего приведет к потере данных.</translation>
+      <translation type="unfinished">There were serious errors while loading the file. Some data might have been modified or not recovered at all. Saving the project will most likely result in loss of data.</translation>
     </message>
     <message>
       <source>Loading %1...</source>
-      <translation>Загрузка %1...</translation>
+      <translation type="unfinished">Loading %1...</translation>
     </message>
   </context>
   <context>
     <name>WebGui::WebView</name>
     <message>
       <source>Open in External Browser</source>
-      <translation>Открыть во внешнем браузере</translation>
+      <translation type="unfinished">Open in External Browser</translation>
     </message>
     <message>
       <source>Open in new window</source>
-      <translation>Открыть в новом окне</translation>
+      <translation type="unfinished">Open in new window</translation>
     </message>
     <message>
       <source>View source</source>
-      <translation>Просмотр HTML-кода</translation>
+      <translation type="unfinished">View source</translation>
     </message>
   </context>
   <context>
     <name>Workbench</name>
     <message>
       <source>Navigation</source>
-      <translation>Навигация</translation>
+      <translation type="unfinished">Navigation</translation>
     </message>
   </context>
 </TS>

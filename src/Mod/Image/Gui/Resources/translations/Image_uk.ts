049dfdce494ac13b1b9ed97d812225edbb95898f
Kuzemko Alexsandr <1382812+Kuzma30@users.noreply.github.com>
2022-05-17 13:44:47 +0300

    New translations Image.ts (Ukrainian)

--- src/Mod/Image/Gui/Resources/translations/Image_uk.ts
+++ src/Mod/Image/Gui/Resources/translations/Image_uk.ts
@@ -2,14 +2,52 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="uk" sourcelanguage="en">
   <context>
+    <name>Dialog</name>
+    <message>
+      <location filename="_CommandImageScaling.py" line="168"/>
+      <source>Scale image plane</source>
+      <translation type="unfinished">Scale image plane</translation>
+    </message>
+    <message>
+      <location filename="_CommandImageScaling.py" line="169"/>
+      <source>Distance [mm]</source>
+      <translation type="unfinished">Distance [mm]</translation>
+    </message>
+    <message>
+      <location filename="_CommandImageScaling.py" line="170"/>
+      <source>Select first point</source>
+      <translation type="unfinished">Select first point</translation>
+    </message>
+    <message>
+      <location filename="_CommandImageScaling.py" line="197"/>
+      <source>&lt;font color=&apos;red&apos;&gt;Enter distance&lt;/font&gt;</source>
+      <translation type="unfinished">&lt;font color=&apos;red&apos;&gt;Enter distance&lt;/font&gt;</translation>
+    </message>
+    <message>
+      <location filename="_CommandImageScaling.py" line="204"/>
+      <source>&lt;font color=&apos;red&apos;&gt;Select ImagePlane&lt;/font&gt;</source>
+      <translation type="unfinished">&lt;font color=&apos;red&apos;&gt;Select ImagePlane&lt;/font&gt;</translation>
+    </message>
+    <message>
+      <location filename="_CommandImageScaling.py" line="235"/>
+      <source>Select second point</source>
+      <translation type="unfinished">Select second point</translation>
+    </message>
+    <message>
+      <location filename="_CommandImageScaling.py" line="250"/>
+      <source>Select Image Plane and type distance</source>
+      <translation type="unfinished">Select Image Plane and type distance</translation>
+    </message>
+  </context>
+  <context>
     <name>Image_Scaling</name>
     <message>
-      <location filename="_CommandImageScaling.py" line="62"/>
+      <location filename="_CommandImageScaling.py" line="69"/>
       <source>Scale image plane</source>
       <translation>Масштабувати зображення площини</translation>
     </message>
     <message>
-      <location filename="_CommandImageScaling.py" line="66"/>
+      <location filename="_CommandImageScaling.py" line="74"/>
       <source>Scales an image plane by defining a distance between two points</source>
       <translation>Масштабує зображення площини, визначаючи відстань між двома точками</translation>
     </message>

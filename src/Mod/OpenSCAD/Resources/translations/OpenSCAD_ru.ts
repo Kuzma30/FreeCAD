6bae9b5e0acdc8857dff5a08109f1d76603b4a91
Kuzemko Alexsandr <1382812+Kuzma30@users.noreply.github.com>
2022-06-21 07:39:39 +0300

    New translations OpenSCAD.ts (Russian)

--- src/Mod/OpenSCAD/Resources/translations/OpenSCAD_ru.ts
+++ src/Mod/OpenSCAD/Resources/translations/OpenSCAD_ru.ts
@@ -2,6 +2,173 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru" sourcelanguage="en">
   <context>
+    <name>Gui::Dialog::DlgSettingsOpenSCAD</name>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="14"/>
+      <source>General settings</source>
+      <translation type="unfinished">General settings</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="35"/>
+      <source>General OpenSCAD Settings</source>
+      <translation type="unfinished">General OpenSCAD Settings</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="43"/>
+      <source>OpenSCAD executable</source>
+      <translation type="unfinished">OpenSCAD executable</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="56"/>
+      <source>The path to the OpenSCAD executable</source>
+      <translation type="unfinished">The path to the OpenSCAD executable</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="74"/>
+      <source>OpenSCAD import</source>
+      <translation type="unfinished">OpenSCAD import</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="82"/>
+      <source>Print debug information in the Console</source>
+      <translation type="unfinished">Print debug information in the Console</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="99"/>
+      <source>If this is checked, Features will claim their children in the tree view</source>
+      <translation type="unfinished">If this is checked, Features will claim their children in the tree view</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="102"/>
+      <source>Use ViewProvider in Tree View</source>
+      <translation type="unfinished">Use ViewProvider in Tree View</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="119"/>
+      <source>If this is checked, Multmatrix Object will be Parametric</source>
+      <translation type="unfinished">If this is checked, Multmatrix Object will be Parametric</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="122"/>
+      <source>Use Multmatrix Feature</source>
+      <translation type="unfinished">Use Multmatrix Feature</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="139"/>
+      <location filename="../ui/openscadprefs-base.ui" line="162"/>
+      <source>The maximum number of faces of a polygon, prism or frustum. If fn is greater than this value the object is considered to be a circular. Set to 0 for no limit</source>
+      <translation type="unfinished">The maximum number of faces of a polygon, prism or frustum. If fn is greater than this value the object is considered to be a circular. Set to 0 for no limit</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="142"/>
+      <source>Maximum number of faces for polygons (fn)</source>
+      <translation type="unfinished">Maximum number of faces for polygons (fn)</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="182"/>
+      <source>Send to OpenSCAD via:</source>
+      <translation type="unfinished">Send to OpenSCAD via:</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="195"/>
+      <source>The transfer mechanism for getting data to and from OpenSCAD</source>
+      <translation type="unfinished">The transfer mechanism for getting data to and from OpenSCAD</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="205"/>
+      <source>Standard temp directory</source>
+      <translation type="unfinished">Standard temp directory</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="210"/>
+      <source>User-specified directory</source>
+      <translation type="unfinished">User-specified directory</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="215"/>
+      <source>stdout pipe (requires OpenSCAD &gt;= 2021.1)</source>
+      <translation type="unfinished">stdout pipe (requires OpenSCAD &gt;= 2021.1)</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="227"/>
+      <source>Transfer directory</source>
+      <translation type="unfinished">Transfer directory</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="240"/>
+      <source>The path to the directory for transferring files to and from OpenSCAD</source>
+      <translation type="unfinished">The path to the directory for transferring files to and from OpenSCAD</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="258"/>
+      <source>OpenSCAD export</source>
+      <translation type="unfinished">OpenSCAD export</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="266"/>
+      <source>maximum fragment size</source>
+      <translation type="unfinished">maximum fragment size</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="286"/>
+      <location filename="../ui/openscadprefs-base.ui" line="296"/>
+      <source>Minimum angle for a fragment</source>
+      <translation type="unfinished">Minimum angle for a fragment</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="289"/>
+      <source>angular (fa)</source>
+      <translation type="unfinished">angular (fa)</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="299"/>
+      <source>°</source>
+      <translation type="unfinished">°</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="328"/>
+      <location filename="../ui/openscadprefs-base.ui" line="353"/>
+      <source>Minimum size of a fragment</source>
+      <translation type="unfinished">Minimum size of a fragment</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="331"/>
+      <source>size (fs)</source>
+      <translation type="unfinished">size (fs)</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="359"/>
+      <source>mm</source>
+      <translation type="unfinished">mm</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="385"/>
+      <source>convexity</source>
+      <translation type="unfinished">convexity</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="422"/>
+      <source>Mesh fallback</source>
+      <translation type="unfinished">Mesh fallback</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="442"/>
+      <location filename="../ui/openscadprefs-base.ui" line="459"/>
+      <source>Deflection</source>
+      <translation type="unfinished">Deflection</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="445"/>
+      <source>deflection</source>
+      <translation type="unfinished">deflection</translation>
+    </message>
+    <message>
+      <location filename="../ui/openscadprefs-base.ui" line="452"/>
+      <source>Triangulation settings</source>
+      <translation type="unfinished">Triangulation settings</translation>
+    </message>
+  </context>
+  <context>
     <name>OpenSCAD</name>
     <message>
       <location filename="../../InitGui.py" line="131"/>
@@ -315,173 +482,6 @@
       <location filename="../../InitGui.py" line="153"/>
       <source>OpenSCADTools</source>
       <translation type="unfinished">OpenSCADTools</translation>
-    </message>
-  </context>
-  <context>
-    <name>Gui::Dialog::DlgSettingsOpenSCAD</name>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="14"/>
-      <source>General settings</source>
-      <translation type="unfinished">General settings</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="35"/>
-      <source>General OpenSCAD Settings</source>
-      <translation type="unfinished">General OpenSCAD Settings</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="43"/>
-      <source>OpenSCAD executable</source>
-      <translation type="unfinished">OpenSCAD executable</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="56"/>
-      <source>The path to the OpenSCAD executable</source>
-      <translation type="unfinished">The path to the OpenSCAD executable</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="74"/>
-      <source>OpenSCAD import</source>
-      <translation type="unfinished">OpenSCAD import</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="82"/>
-      <source>Print debug information in the Console</source>
-      <translation type="unfinished">Print debug information in the Console</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="99"/>
-      <source>If this is checked, Features will claim their children in the tree view</source>
-      <translation type="unfinished">If this is checked, Features will claim their children in the tree view</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="102"/>
-      <source>Use ViewProvider in Tree View</source>
-      <translation type="unfinished">Use ViewProvider in Tree View</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="119"/>
-      <source>If this is checked, Multmatrix Object will be Parametric</source>
-      <translation type="unfinished">If this is checked, Multmatrix Object will be Parametric</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="122"/>
-      <source>Use Multmatrix Feature</source>
-      <translation type="unfinished">Use Multmatrix Feature</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="139"/>
-      <location filename="../ui/openscadprefs-base.ui" line="162"/>
-      <source>The maximum number of faces of a polygon, prism or frustum. If fn is greater than this value the object is considered to be a circular. Set to 0 for no limit</source>
-      <translation type="unfinished">The maximum number of faces of a polygon, prism or frustum. If fn is greater than this value the object is considered to be a circular. Set to 0 for no limit</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="142"/>
-      <source>Maximum number of faces for polygons (fn)</source>
-      <translation type="unfinished">Maximum number of faces for polygons (fn)</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="182"/>
-      <source>Send to OpenSCAD via:</source>
-      <translation type="unfinished">Send to OpenSCAD via:</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="195"/>
-      <source>The transfer mechanism for getting data to and from OpenSCAD</source>
-      <translation type="unfinished">The transfer mechanism for getting data to and from OpenSCAD</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="205"/>
-      <source>Standard temp directory</source>
-      <translation type="unfinished">Standard temp directory</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="210"/>
-      <source>User-specified directory</source>
-      <translation type="unfinished">User-specified directory</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="215"/>
-      <source>stdout pipe (requires OpenSCAD &gt;= 2021.1)</source>
-      <translation type="unfinished">stdout pipe (requires OpenSCAD &gt;= 2021.1)</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="227"/>
-      <source>Transfer directory</source>
-      <translation type="unfinished">Transfer directory</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="240"/>
-      <source>The path to the directory for transferring files to and from OpenSCAD</source>
-      <translation type="unfinished">The path to the directory for transferring files to and from OpenSCAD</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="258"/>
-      <source>OpenSCAD export</source>
-      <translation type="unfinished">OpenSCAD export</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="266"/>
-      <source>maximum fragment size</source>
-      <translation type="unfinished">maximum fragment size</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="286"/>
-      <location filename="../ui/openscadprefs-base.ui" line="296"/>
-      <source>Minimum angle for a fragment</source>
-      <translation type="unfinished">Minimum angle for a fragment</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="289"/>
-      <source>angular (fa)</source>
-      <translation type="unfinished">angular (fa)</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="299"/>
-      <source>°</source>
-      <translation type="unfinished">°</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="328"/>
-      <location filename="../ui/openscadprefs-base.ui" line="353"/>
-      <source>Minimum size of a fragment</source>
-      <translation type="unfinished">Minimum size of a fragment</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="331"/>
-      <source>size (fs)</source>
-      <translation type="unfinished">size (fs)</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="359"/>
-      <source>mm</source>
-      <translation type="unfinished">mm</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="385"/>
-      <source>convexity</source>
-      <translation type="unfinished">convexity</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="422"/>
-      <source>Mesh fallback</source>
-      <translation type="unfinished">Mesh fallback</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="442"/>
-      <location filename="../ui/openscadprefs-base.ui" line="459"/>
-      <source>Deflection</source>
-      <translation type="unfinished">Deflection</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="445"/>
-      <source>deflection</source>
-      <translation type="unfinished">deflection</translation>
-    </message>
-    <message>
-      <location filename="../ui/openscadprefs-base.ui" line="452"/>
-      <source>Triangulation settings</source>
-      <translation type="unfinished">Triangulation settings</translation>
     </message>
   </context>
 </TS>
